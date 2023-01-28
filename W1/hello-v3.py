dec30d767eae09574fe1bd448de7d760977fa86d
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-28 21:12:07 +0530

    feat: 🌱 use f-string and escape sequence

---
+++ W1/hello-v3.py
@@ -0,0 +1,9 @@
+# Ask user for input
+name = input("Your name please? ")
+
+# New feature: format string or f-string
+print(f"Hello," {name})
+
+# Use escape sequence with and without f-string
+print("Hello, \"Danish\"")
+print(f"Hello, \"{name}\"")
