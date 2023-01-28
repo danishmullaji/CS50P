b037f2b2d7c1dc45eb051a9004f7a40003052739
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-28 21:14:40 +0530

    feat: ✨ use strip and capitalize method

---
+++ W1/hello-v4.py
@@ -0,0 +1,6 @@
+# Get the user input and store the value
+# Make it clean, capitalize and remove the unwanted space
+# Combinding two methods togther to clean code
+name = input("Tell me your name: ").strip().capitalize()
+
+print(f"Hello, {name}")
