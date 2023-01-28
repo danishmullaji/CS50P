ac39d21ae768be720ea3c68a17dd86bce8321af4
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-28 22:18:10 +0530

    refactor/style: ♻️💡 add round function and comment

--- W1/methods.py
+++ W1/methods.py
@@ -10,11 +10,18 @@ name = name.capitalize()
 # Method: It capitalize the first letter of all the words mentioned of the input
 name = name.title()
 
-# Method: It split the string into short substring and it can be use separately
+# Method: It split the string into short substring and it can be use separately 
 first, last = name.split()
 
 # Method: It replace the provided value with the assigned value
 name = name.replace(" ", "...")
 
+# Method: It round-off the numbers to the nearest value
+digit_one = 1010.59
+digit_two = 1010.64
+
+summation = round(digit_one + digit_two, 2)
+
 # Use print function
-print(f"Hello, {name}")
+print(f"Hello, {first}")
+print(f"Total: {summation:,}")
