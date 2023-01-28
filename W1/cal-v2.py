83c31b8998c7fc61150a3f4e1e7639d913cda476
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-28 21:19:53 +0530

    refactor: ♻️ use nest to clean code

---
+++ W1/cal-v2.py
@@ -0,0 +1,7 @@
+# Ask user input for two variables
+# Nest the input with integer 
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# Use print function
+print(f"Combined value: {x + y}")
