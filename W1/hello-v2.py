c847c7fedc70eab0201c945e43abcef9a146e64a
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-28 21:09:54 +0530

    refactor: ♻️ use sep and end in print function

---
+++ W1/hello-v2.py
@@ -0,0 +1,7 @@
+# Asking user for the input
+name = input("Enter your name: ")
+
+# print(*object, sep='', end="\n") - sep: provide default space, end: provide default new line, its called named parameter
+# print("Hello,", name, sep="") - Removes the space
+print("Hello, ", end="")
+print(name)
