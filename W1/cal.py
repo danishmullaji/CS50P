4ae31f88312684d72416855ac79115daee736ee1
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-28 21:19:05 +0530

    feat: 🌱 initial commit

---
+++ W1/cal.py
@@ -0,0 +1,9 @@
+# Get user input for two variables
+x = input("Value for x: ")
+y = input("Value for y: ")
+
+# Arithmatic addition of the variables
+z = int(x) + int(y)
+
+# Use print the Summation
+print(f"Total value is {z}")
