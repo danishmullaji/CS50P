f6d865e85ee379461606fc3aeb1905be43f52773
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-30 11:24:05 +0000

    refactor: ♻️ use != function

---
+++ W1/compare-v4.py
@@ -0,0 +1,10 @@
+# Ask user input
+a = int(input("Your value of a: "))
+b = int(input("Your value of b: "))
+
+# Use or function
+# Use print function
+if a != b:
+    print("a and b are not equal")
+else:
+    print("a and b are equal")
