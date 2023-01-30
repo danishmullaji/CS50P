ca0a9610ca8e2f6119406e481b007badbe1e75c8
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-30 11:21:03 +0000

    feat: ✨ use or function

---
+++ W1/compare-v3.py
@@ -0,0 +1,10 @@
+# Ask user input
+a = int(input("Your value of a: "))
+b = int(input("Your value of b: "))
+
+# Use or function
+# Use print function
+if a < b or b > a:
+    print("a and b are not equal")
+else:
+    print("a and b are equal")
