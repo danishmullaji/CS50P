627fc44ee259ad2644861e04b20d3c2157b5c956
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-30 10:37:19 +0000

    feat: ✨ initial commit

---
+++ W1/compare.py
@@ -0,0 +1,12 @@
+# Ask user input
+a = int(input("Your value of a: "))
+b = int(input("Your value of b: "))
+
+# Use if function
+# Use print function
+if a < b:
+    print("a is smaller than b")
+if a > b:
+    print("a is greater than b")
+if a == b:
+    print("a and b are equal")
