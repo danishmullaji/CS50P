e7dfac653db64f2abbc6122f3b5baab10e70f397
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-30 12:48:19 +0000

    refactor: ♻️ clean code

---
+++ W1/grade-v2.py
@@ -0,0 +1,14 @@
+# Ask User input
+score = int(input("Enter your score: "))
+
+# Write condition for if and elif
+if 75 <= score <=100:
+    print("Grade: A - Excellent")
+elif 60 <= score < 75:
+    print("Grade: B - Satisfactory")
+elif 45 <= score < 60:
+    print("Grade: C - Good")
+elif 35 <= score < 45:
+    print("Grade: D - Poor")
+else:
+    print("Grade: F - Fail")
