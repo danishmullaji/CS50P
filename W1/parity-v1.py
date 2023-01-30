90060cc2b53a527e2d86359494fa44a645bd4ec4
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-30 17:08:30 +0000

    feat: ✨ initial commit

---
+++ W1/parity-v1.py
@@ -0,0 +1,8 @@
+# Ask user input
+digit = int(input("Mention the number: "))
+
+# Create if, else function for even and odd
+if digit % 2 == 0:
+    print(f"{digit} is a even number")
+else:
+    print(f"{digit} is an odd number")
