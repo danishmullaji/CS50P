f67164bd0f1411b2972bd85db8be7a5fa3caae92
danishmullaji <123380962+danishmullaji@users.noreply.github.com>
2023-01-30 10:46:49 +0000

    refactor: ♻️ use elif function

---
+++ W1/compare-v2.py
@@ -0,0 +1,12 @@
+# Ask user input
+a = int(input("Your value of a: "))
+b = int(input("Your value of b: "))
+
+# Use if, elif and else function
+# Use print function
+if a < b:
+    print("a is smaller than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a and b are equal")
