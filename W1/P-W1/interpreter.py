78e1225ca6591472de80c4096199b54344f2cfae
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-01 17:48:07 +0000

    feat: 🌱 initial commit

---
+++ W1/P-W1/interpreter.py
@@ -0,0 +1,13 @@
+# Ask user input for three variable
+x, y, z = input().split()
+
+# Use if elif else statement for calculation
+if y == "+":
+    sum = int(x) + int(z)
+    print(float(sum))
+elif y == "-":
+    print(float(int(x) - int(z)))
+elif y == "*":
+    print(float(int(x) * int(z)))
+elif y == "/":
+    print(float(int(x) / int(z)))
