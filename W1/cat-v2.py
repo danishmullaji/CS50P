94927c2c847d5cdefef96499583d827fee507d57
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-04 06:14:47 +0000

    feat/refacter: ✨♻️ use for loop

---
+++ W1/cat-v2.py
@@ -0,0 +1,6 @@
+# Use for loop 
+#for repeat in [0, 1, 2]: - use list to provide repeat value
+#for repeat in range(3): - use range function to provide repeat value
+# use _ for variable initialization if using once
+for _ in range(3):
+    print("Meow")
