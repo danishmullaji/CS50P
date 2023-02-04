b63e4e4f1e4104c76abc726f15cbc468bc323927
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-04 06:56:31 +0000

    refactor: ♻️ use while for loop with if statement

---
+++ cat-v3.py
@@ -0,0 +1,13 @@
+# Create a while loop to validate the input
+while(True):
+    n = int(input("What is n? "))
+    if n > 0:
+        break 
+    # if n < 0:
+      #  continue
+    # else:
+      #  break
+
+# Use for loop to print the output with provided input
+for _ in range(n):
+    print("Meow")
