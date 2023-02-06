aa1acb246aa140edb6d65a6629a643c138a9c857
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-06 12:11:41 +0000

    feat/refactor: ✨♻ use len function

---
+++ W3/thrones-v2.py
@@ -0,0 +1,7 @@
+# Create a list
+houses = ["Targaryen", "Lannister", "Greyjoy", "Baratheon", "Stark", "Martell", "Arryn"]
+
+# Use for loop to display the list
+#print(houses[0])
+for house in range(len(houses)):
+    print(house + 1, houses[house])
