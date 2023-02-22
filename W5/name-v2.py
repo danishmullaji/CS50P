d7f0a00978e0ee54f73359e15381eef39c4d14e0
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-22 12:53:42 +0000

    chore: 🚚 rename file

--- W5/name-v2.py
+++ W5/name-v2.py
@@ -6,8 +6,3 @@ elif len(sys.argv) > 2:
     sys.exit("If mentioning your fullname use \"\"")
 
 print(f"Hello, {sys.argv[1]}")
-
-"""try:
-    print(f"Hello, {sys.argv[1]}")
-except IndexError:
-    print("Mention your name while running file")"""
