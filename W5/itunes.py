116c0274d1fe531f3b6a1a601294fc3e8f36f7bd
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-22 14:16:33 +0000

    feat: ✨ use requests library to fetch data

---
+++ W5/itunes.py
@@ -0,0 +1,13 @@
+import sys
+import json
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit("Mention your name while running file")
+
+request = requests.get(f"https://itunes.apple.com/search?entity=song&limit=10&term= {sys.argv[1]}")
+
+json = request.json()
+
+for result in json["results"]:
+    print(result["trackName"])
