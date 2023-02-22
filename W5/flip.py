842f32467c73c266f32abcad03bf7c55faefe0bf
Danish Mullaji <123380962+danishmullaji@users.noreply.github.com>
2023-02-22 10:37:48 +0000

    feat: ✨ use import and from keyword

---
+++ W5/flip.py
@@ -0,0 +1,6 @@
+# import random - Its use to import everything in random library
+# from random - It only import the mentioned function
+from random import choice
+
+coin = choice(["Heads", "Tails"])
+print(coin)
